5d48d1330f56f04d7d07be707a6e9cc313d9dbb8
Stoyan83 <stoyan_stoyanov@rocketmail.com>
2023-11-20 17:09:09 +0200

    User authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  include DeviseTokenAuth::Concerns::SetUserByToken
+
+  protect_from_forgery with: :null_session, if: -> { request.format.json? }
+  respond_to :json
 end
