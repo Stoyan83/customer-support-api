92a22c10e019d17f7b2e6748a738f1041a9fa3cb
Stoyan83 <stoyan_stoyanov@rocketmail.com>
2023-11-20 23:44:28 +0200

    return token in body

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,11 @@ class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
 
   protect_from_forgery with: :null_session, if: -> { request.format.json? }
-  respond_to :json
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    # Permit additional parameters for the sign-in action, including nested `:session` parameter
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:session, { session: %i[email password] }])
+  end
 end
